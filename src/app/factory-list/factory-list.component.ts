cb1536bf410ac71b7be23f6722375d31a38b0375
Kramer <Michael.Kramer@bnymellon.com>
2018-08-10 12:56:16 -0400

    frontend jpsite

---
+++ src/app/factory-list/factory-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-factory-list',
+  templateUrl: './factory-list.component.html',
+  styleUrls: ['./factory-list.component.css']
+})
+export class FactoryListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
